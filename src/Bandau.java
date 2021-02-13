18d2c2ef970195c50c3d048934429f7dd8e83c42
SandraStan <sandra.stankeviciene@gmail.com>
2021-02-13 10:00:17 +0200

    first commit

---
+++ src/Bandau.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Bandau {
+    public static void main (String[] args) {
+
+        System.out.println("as");
+
+
+
+    }
+
+
+
+
+}
---
+++ src/scanner/Skaneris.java
@@ -0,0 +1,19 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Skaneris {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int ska1 = scanner.nextInt();
+
+        System.out.println(ska1);
+
+
+
+    }
+
+}
